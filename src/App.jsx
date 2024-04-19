c73dee01621963f4545bab823c17943cc8bff680
Demo class <aks6403861@gmail.com>
2024-04-19 19:31:18 +0530

    Initial commit message

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Lottery from "./Lottery.jsx";
+import "./App.css";
+import {sum} from "./helper.js";
+
+function App() {
+  let winCondition = (ticket) => {
+    return sum(ticket) === 15;
+  };
+  return (
+    <>
+     <Lottery n={3} winCondition={winCondition}/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Lottery.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"; 
+import {genTicket, sum} from "./helper.js";
+import Ticket from "./Ticket";
+
+export default function Lottery({n = 3, winCondition}) {
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = winCondition(ticket);
+
+    let buyTicket = () =>{
+        setTicket(genTicket(n))
+    }
+    return (
+        <div>
+            <h1>Lottery Games!</h1>
+            <Ticket ticket={ticket}/>
+            <br></br>
+            <button onClick={buyTicket}>Buy New Ticket</button>
+            <h3 className="winning">{isWinning && "Congratulation, You won!"}</h3>
+        </div>
+    )
+}
